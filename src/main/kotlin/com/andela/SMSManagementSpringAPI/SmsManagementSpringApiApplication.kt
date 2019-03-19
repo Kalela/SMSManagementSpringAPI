a81c7b4e5857bf1b5ae01e81e92e79d784dea4db
Kalela <philipkalela@gmail.com>
2019-03-19 13:32:28 +0300

    Initial commit

---
+++ src/main/kotlin/com/andela/SMSManagementSpringAPI/SmsManagementSpringApiApplication.kt
@@ -0,0 +1,11 @@
+package com.andela.SMSManagementSpringAPI
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmsManagementSpringApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmsManagementSpringApiApplication>(*args)
+}
