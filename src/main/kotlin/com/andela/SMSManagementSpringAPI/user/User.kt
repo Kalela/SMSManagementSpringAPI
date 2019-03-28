d42805fefe5262da77599f2c21c892592f53eab2
Kalela <philipkalela@gmail.com>
2019-03-28 13:23:00 +0300

    feature(registration): set up user registration

--- src/main/kotlin/com/andela/SMSManagementSpringAPI/user/User.kt
+++ src/main/kotlin/com/andela/SMSManagementSpringAPI/user/User.kt
@@ -1,18 +1,16 @@
 package com.andela.SMSManagementSpringAPI.user
 
-import com.andela.SMSManagementSpringAPI.util.message.models.Message
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import javax.persistence.*
 
 @Entity
-class User(val recieverName: String) {
+@JsonIgnoreProperties("hibernateLazyInitializer", "handler")
+@Table(name = "users")
+class User(val username: String) {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id: Long = 0
 
-    fun getMessage(message: Message) {
+    val password: String = ""
 
-    }
 }
--- src/main/kotlin/com/andela/SMSManagementSpringAPI/user/UserController.kt
+++ src/main/kotlin/com/andela/SMSManagementSpringAPI/user/UserController.kt
@@ -1,14 +1,37 @@
 package com.andela.SMSManagementSpringAPI.user
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/v1")
-class UserController(val userRepository: UserRepository) {
+class UserController {
+
+    @Autowired
+    lateinit var userRepository: UserRepository
 
     @GetMapping
     fun getSomething() : String = "Look at me"
+
+    @PostMapping("/sendmessage")
+    fun sendMessage(): String {
+//        userRepository.findById(1)
+        return "Message sent"
+    }
+
+    @PostMapping("/auth/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun registerUser(@Valid @RequestBody user : User ): String {
+        try {
+            userRepository.save(user)
+        } catch (e : Exception) {
+            return "Something went wrong"
+        }
+
+        println(user.username + user.id)
+        return "User registered successfully"
+    }
 
 }
